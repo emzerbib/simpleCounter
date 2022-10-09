72ada64fe1158a295bbe46a5a3659afbc6f22975
Ubuntu <emile@vm-perso.jca1i2mvsmvejlukwtmwaqsyyg.bx.internal.cloudapp.net>
2022-10-09 18:37:53 +0000

    add post method to main.py and create .gitignore file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI, Request
+from itertools import count
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
+import models
+from models import CounterTable, CountChangeRequest
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
 templates = Jinja2Templates(directory="templates")
+
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home(request: Request):
@@ -15,3 +32,22 @@ def home(request: Request):
         "somevar": 2
     })
 
+
+def fetch_stock_data(id: int):
+    pass
+
+@app.post("/change")
+def change(change_request: CountChangeRequest, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+
+    countertable = CounterTable()
+    countertable.count = change_request.change
+
+    db.add(countertable)
+    db.commit()
+    background_tasks.add_task(fetch_stock_data, countertable.id)
+    
+
+    return {
+        "code": "success",
+        "message": "read to make changes"
+    }
