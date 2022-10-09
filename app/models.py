ea141c29d0d8ef90e910f2b63dd881b546add083
Ubuntu <emile@vm-perso.jca1i2mvsmvejlukwtmwaqsyyg.bx.internal.cloudapp.net>
2022-10-09 18:30:06 +0000

    add counter db model and some request model

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from cgi import parse_multipart
+from enum import Enum
+from sqlalchemy import Boolean, Column, Integer, ForeignKey, String
+from database import Base
+from pydantic import BaseModel
+
+
+class CounterTable(Base):
+    __tablename__ = "counts"
+    id = Column(Integer, primary_key=True, index=True)
+    count = Column(Integer, primary_key=False, index=False)
+
+class IncrementalChange(Enum):
+    add = 1
+    substract = -1
+
+
+class CountChangeRequest(BaseModel):
+    change: IncrementalChange
