cdc17dcca65db446ffbb3ca9ab363e5884332521
Ubuntu <emile@vm-marcel.coxbucwbobaudm2xz5p44uaqzb.bx.internal.cloudapp.net>
2022-10-10 22:18:03 +0000

    change path for config file

--- app/main.py
+++ app/main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
-from utils import add_to_counter, substract_from_counter, update_config
+#from simpleCounter.app.utils import add_to_counter, substract_from_counter, update_config
 import configparser
+import uvicorn
+
+import os 
+print(os.listdir('.'))
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('app/config.ini')
 count_str = config['variables']['count']
 count = int(count_str)
 
+
+def add_to_counter():
+    global count
+    count += 1
+
+def substract_from_counter():
+    global count
+    count -= 1
+
+def update_config(count):
+    global config 
+    config['variables']['count'] = str(count)
+    with open('app/config.ini', 'w') as configfile:
+        config.write(configfile)
 
 @app.get("/")
 def home(request: Request):
@@ -42,3 +60,7 @@ async def substract(request: Request, background_tasks: BackgroundTasks):
         "request": request,
         "data": count
     })
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host="0.0.0.0")
