3da0502654810a87517df4d5856a5ca7bb75afda
Ubuntu <emile@vm-marcel.coxbucwbobaudm2xz5p44uaqzb.bx.internal.cloudapp.net>
2022-10-10 18:49:00 +0000

    delete database.py, models.py, clean up main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.templating import Jinja2Templates
-import models
-from models import CounterTable, Addition, Substraction
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from utils import add_to_counter, substract_from_counter, update_config
 import configparser
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
-
 templates = Jinja2Templates(directory="templates")
-
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 count_str = config['variables']['count']
@@ -30,24 +23,8 @@ def home(request: Request):
         "data": count
     })
 
-def add_to_counter():
-    global count
-    count += 1
-
-def substract_from_counter():
-    global count
-    count -= 1
-
-def update_config(count):
-    global config 
-    config['variables']['count'] = str(count)
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-
 @app.post("/add")
 async def add(request: Request, background_tasks: BackgroundTasks):
-
-
     add_to_counter()
     background_tasks.add_task(update_config, count)
 
@@ -58,12 +35,6 @@ async def add(request: Request, background_tasks: BackgroundTasks):
 
 @app.post("/substract")
 async def substract(request: Request, background_tasks: BackgroundTasks):
-
-    #background_tasks.add_task(add)
-
-    #global count
-    #count -= 1
-
     substract_from_counter()
     background_tasks.add_task(update_config, count)
 
@@ -71,4 +42,3 @@ async def substract(request: Request, background_tasks: BackgroundTasks):
         "request": request,
         "data": count
     })
-
